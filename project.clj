64f3b643cab7f198a6673b7c453934daa2098f9f
Rodrigo Flores <mail@rodrigoflores.org>
2016-01-03 15:50:21 -0200

    Apache V2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject app-with-midje/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Generates a Clojure app with midje and schema"
+  :url "https://github.com/rodrigoflores/app-with-midje"
+  :license {:name "Apache V2"
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :eval-in-leiningen true)
